7c318cbd6af10a3b4d1bbe9b60f5782835749801
naflazatihulwani <naflazatihulwani5@gmail.com>
2026-05-22 14:11:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 app naflacantik")
+st.title("🎈 Nafla app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
